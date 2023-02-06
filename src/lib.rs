1363e0dced5844ff14598dbb7841e09f1dfa5d11
Henk Oordt <hd@oordt.dev>
2023-02-06 19:59:08 +0100

    Implement FromIter and Extend, start on writing instructions

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,33 @@
 use std::{
-    ops::{Index, IndexMut, Range, RangeFrom, RangeTo},
+    ops::{Deref, DerefMut, Index, IndexMut, Range, RangeFrom, RangeTo},
     slice::SliceIndex,
 };
+
+/// Steps
+/// 0. Intro: Let's create a resizable buffer called `LocalStorageVec` that lives on the stack if it's short,
+/// but is moved to the heap if it needs to grow large. On the stack, the
+/// buffer is backed by a fixed-size array, and on the heap it's backed by a dynamically-sized `Vec`. 
+/// Q1: in which case might such a type provice a perforance boost compared to `Vec`?
+/// Q2: At what cost does this boost come, compared to `Vec`?
+/// 1. Define const BUF_SIZE = 16. Define enum with variants Stack and Heap. 
+/// Stack variant is struct-like, contains fields `len: usize` and `buf: [i32, BUF_SIZE]`
+/// 2. Implement methods `new`, `len`, and `with_capacity`
+/// 3. Introduce type generic param `T`, and bound the type `T: Default + Copy`. 
+/// This allows us to initialize the buffer using the `[T::default(); BUF_SIZE]` syntax; Apart from making the buffer
+/// less flexible in use, this comes at a performance cost we need to instantiate a default value of `T` for each slot in the buffer array.
+/// This also prevents us from using the buffer with types that are `Drop`, as types that are `Copy` cannot be `Drop`.
+/// 4. In the previous step, you've introduced what is called a 'type generic parameter'. To make the buffer more versatile, 
+/// we will introduce a 'const generic parameter' as well. A const generic parameter is a parameter that repersent a constant. In our case,
+/// it can represent the size of the array that acts as the stack buffer and can therefore replace `BUF_SIZE`. This allows
+/// the user of the `LocalStorageVec` to decide how many items can fit on the stack-based variant of the buffer.
+/// 5. Implement `From<Vec<T>>` for `LocalStorageVec<T>`. As the `Vec` is already on the heap, 
+/// this `From` implementation simply returns the heap-backed variant of the `LocalStorageVec` into which the passed `Vec` is moved.
+/// 6. Implmement `push` and `pop`. `push` appends one element to the end of the buffer, and `pop` removes and returns the last element.
+/// Within `push`, if the buffer is backed by an array on the stack, the contents are moved to the heap before the element is appended.
+/// If `pop` causes the length of the buffer to become smaller than `N`, it still won't move
+/// any data from the heap over to the stack. In other words, a heap-backed variant is never turned into a stack-based variant, even though
+/// the opposite transition sometimes occur.
+/// 7. Implement From<[T; N]>
 
 #[derive(Debug, PartialEq, Eq, Clone)]
 pub enum LocalStorageVec<T: Default + Copy, const N: usize> {
@@ -10,6 +36,7 @@ pub enum LocalStorageVec<T: Default + Copy, const N: usize> {
 }
 
 impl<T: Default + Copy, const N: usize> LocalStorageVec<T, N> {
+    // Done
     pub fn new() -> Self {
         Self::Stack {
             buf: [Default::default(); N],
@@ -17,9 +44,19 @@ impl<T: Default + Copy, const N: usize> LocalStorageVec<T, N> {
         }
     }
 
+    // Done
+    pub fn with_capacity(capacity: usize) -> Self {
+        if capacity <= N {
+            Self::new()
+        } else {
+            Self::Heap(Vec::with_capacity(capacity))
+        }
+    }
+
+    // Done
     pub fn push(&mut self, value: T) {
         match self {
-            LocalStorageVec::Stack { buf, len } if *len < 128 => {
+            LocalStorageVec::Stack { buf, len } if *len < N => {
                 buf[*len] = value;
                 *len += 1;
             }
@@ -33,6 +70,7 @@ impl<T: Default + Copy, const N: usize> LocalStorageVec<T, N> {
         }
     }
 
+    // Done
     pub fn pop(&mut self) -> Option<T> {
         match self {
             LocalStorageVec::Stack { buf, len } if *len > 0 => {
@@ -82,6 +120,7 @@ impl<T: Default + Copy, const N: usize> LocalStorageVec<T, N> {
         }
     }
 
+    // Done
     pub fn len(&self) -> usize {
         match self {
             LocalStorageVec::Stack { len, .. } => *len,
@@ -111,6 +150,20 @@ impl<T: Default + Copy, const N: usize, const M: usize> From<[T; N]> for LocalSt
             let v = value.to_vec();
             Self::Heap(v)
         }
+    }
+}
+
+impl<T: Default + Copy, const N: usize> Deref for LocalStorageVec<T, N> {
+    type Target = [T];
+
+    fn deref(&self) -> &Self::Target {
+        self.as_ref()
+    }
+}
+
+impl<T: Default + Copy, const N: usize> DerefMut for LocalStorageVec<T, N> {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        self.as_mut()
     }
 }
 
@@ -179,13 +232,30 @@ impl<T: Default + Copy, const N: usize> ExactSizeIterator for StackVecIter<T, N>
 
 impl<T: Default + Copy, const N: usize> FromIterator<T> for LocalStorageVec<T, N> {
     fn from_iter<I: IntoIterator<Item = T>>(iter: I) -> Self {
-        // TODO
+        let iter = iter.into_iter();
+        let (size_min, size_max) = iter.size_hint();
+        let mut vec = Self::with_capacity(size_max.unwrap_or(size_min));
+        match &mut vec {
+            LocalStorageVec::Stack { .. } => vec.extend(iter),
+            LocalStorageVec::Heap(v) => v.extend(iter),
+        }
+        vec
     }
 }
 
 impl<T: Default + Copy, const N: usize> Extend<T> for LocalStorageVec<T, N> {
     fn extend<I: IntoIterator<Item = T>>(&mut self, iter: I) {
-        // TODO
+        let iter = iter.into_iter();
+        let (size_min, size_max) = iter.size_hint();
+        let mut vec = Self::with_capacity(size_max.unwrap_or(size_min));
+        match self {
+            LocalStorageVec::Stack { .. } => {
+                for item in iter {
+                    vec.push(item);
+                }
+            }
+            LocalStorageVec::Heap(v) => v.extend(iter),
+        }
     }
 }
 
@@ -194,7 +264,7 @@ mod test {
     use crate::LocalStorageVec;
 
     #[test]
-    #[cfg(feature = "disabled")]
+    // #[cfg(feature = "disabled")]
     fn it_pushes() {
         let mut vec: LocalStorageVec<_, 128> = LocalStorageVec::new();
         for value in 0..128 {
@@ -216,7 +286,7 @@ mod test {
     }
 
     #[test]
-    #[cfg(feature = "disabled")]
+    // #[cfg(feature = "disabled")]
     fn it_pops() {
         let mut vec: LocalStorageVec<_, 128> = LocalStorageVec::from([0; 128]);
         for _ in 0..128 {
@@ -238,7 +308,7 @@ mod test {
     }
 
     #[test]
-    #[cfg(feature = "disabled")]
+    // #[cfg(feature = "disabled")]
     fn it_inserts() {
         let mut vec: LocalStorageVec<_, 4> = LocalStorageVec::from([0, 1, 2]);
         vec.insert(1, 3);
@@ -262,7 +332,7 @@ mod test {
     }
 
     #[test]
-    // #[cfg(feature = "disabled")]
+    #[cfg(feature = "disabled")]
     fn it_removes() {
         let mut vec: LocalStorageVec<_, 4> = LocalStorageVec::from([0, 1, 2]);
         let elem = vec.remove(1);
@@ -284,7 +354,7 @@ mod test {
     }
 
     #[test]
-    #[cfg(feature = "disabled")]
+    // #[cfg(feature = "disabled")]
     fn it_iters() {
         let vec: LocalStorageVec<_, 128> = LocalStorageVec::from([0; 128]);
         let mut iter = vec.into_iter();
